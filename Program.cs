46f84ff014565f637b111ec73a98e74f19f04c55
William Santana de Amorim <williamsantanadeamorim@MacBook-Air-de-William.local>
2024-09-22 21:14:45 -0400

    first commit

---
+++ Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+class Program
+{
+    delegate void ProcessString(string input);
+
+    static void Main(string[] args)
+    {
+        ProcessString toUpperCase = delegate (string input)
+        {
+            Console.WriteLine(input.ToUpper());
+        };
+
+        ProcessString toLowerCase = delegate (string input)
+        {
+            Console.WriteLine(input.ToLower());
+        };
+
+        Console.Write("Digite uma frase: ");
+        string input = Console.ReadLine();
+
+        toUpperCase(input);
+        toLowerCase(input);
+    }
+}
